feae4d5bbd8e5b91efc3a36ce052eb3831a1331b
Abhranil Singha Roy <abhranilsingharoy@gmail.com>
2026-06-15 11:52:12 +0530

    fix: Bump SW version to v5 and use inline SVG in chat to fix subfolder caching issues

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <div class="chat-widget" id="zap-chat-widget">
       <div class="chat-window" id="zap-chat-window">
         <div class="chat-header">
-          <h3><img src="assets/icons/lightning.svg" width="16" height="16"> ZapBot</h3>
+          <h3><svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="color: var(--color-primary);"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> ZapBot</h3>
           <button class="chat-close" id="zap-chat-close">&times;</button>
         </div>
         <div class="chat-messages" id="zap-chat-messages">
